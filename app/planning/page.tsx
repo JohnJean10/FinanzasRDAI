cd2be869de99e7b27719ce176b2e07ea9a9a57ee
FinanzasRD Developer <dev@finanzasrd.local>
2026-01-12 15:43:29 -0400

    fix: corregir espaciado de la página de Presupuestos

--- app/planning/page.tsx
+++ app/planning/page.tsx
@@ -1,9 +1,5 @@
 import BudgetManager from "@/components/features/planning/BudgetManager";
 
 export default function PlanningPage() {
-    return (
-        <div className="p-2 md:p-4">
-            <BudgetManager />
-        </div>
-    );
+    return <BudgetManager />;
 }
